fa3aa0c8fed4c33a4b028c84c7ccbf731ddee6f5
ahcclubiium <ahcclubiium@gmail.com>
2024-07-06 01:23:26 +0800

    Update page.jsx

--- app/program_cms/page.jsx
+++ app/program_cms/page.jsx
@@ -5,8 +5,8 @@ import BlogPostCard from '../components/BlogPostCard';
 import Heading from '../components/Heading';
 import { createClient } from 'contentful';
 
-const CONTENTFUL_SPACE_ID = process.env.REACT_APP_CONTENTFUL_SPACE_ID;
-const CONTENTFUL_ACCESS_TOKEN = process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN;
+// const CONTENTFUL_SPACE_ID = process.env.REACT_APP_CONTENTFUL_SPACE_ID;
+// const CONTENTFUL_ACCESS_TOKEN = process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN;
 
 export default function Page() {
   const [entries, setEntries] = useState([]);
