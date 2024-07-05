4e44212006fe0d1d3b5e188f1d626a537ee2b717
PlashSpeed-Aiman <62431177+PlashSpeed-Aiman@users.noreply.github.com>
2024-07-06 01:46:40 +0800

    Update next.config.mjs
    kuhkuh

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
     ],
   },
   // env: {
-  //   CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+  //   CONTuhuhuENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
   //   CONTENTFUL_ACCESS_KEY: process.env.CONTENTFUL_ACCESS_KEY,
   // },
 };
